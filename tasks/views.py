3b81d8ae7ce9dd55c08529d336e431b6c110fe1c
Nabil <nabilsafowan936@gmail.com>
2025-06-29 20:01:50 +0600

    Updated

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from tasks.forms import TaskForm, TaskModelForm
-from tasks.models import Employee,Task
+from tasks.models import *
+from datetime import date
+from django.db.models import Q,Count
+
 # Create your views here.
 
 def manager_dashboard(request):
@@ -52,23 +55,71 @@ def create_task(request):
     return render(request, "task_form.html", context)
 
 def view_task(request):
-    # retrieve all data from tasks model
+    # All tasks
     tasks = Task.objects.all()
 
-    # retrieve a specific task
-    task_3 = Task.objects.get(id=1)
+    # Single task with id=1
+    try:
+        task_1 = Task.objects.get(id=1)
+    except Task.DoesNotExist:
+        task_1 = None
 
-    # retrieve a specific task using filter
-    task_2 = Task.objects.filter(id=2)
+    # Tasks with status "PENDING"
+    task_2 = Task.objects.filter(status="PENDING")
 
-    # fetch the first task
+    # First task
     first_task = Task.objects.first()
+
+    # Tasks whose due date is today
+    today_task = Task.objects.filter(due_date=date.today())
+
+    # TaskDetail with priority not LOW
+    priority_task = TaskDetail.objects.exclude(priority='L')
+
+    #query for show the task that contains word 'c'
+    tasks_with_c = Task.objects.filter(title__icontains="c")
+
+    # New query for PENDING or IN_PROGRESS
+    tasks_pending_or_inprogress = Task.objects.filter(
+        Q(status='PENDING') | Q(status='IN_PROGRESS')
+    )
+    # QuerySet method that returns True or False
+    exists_or_not = Task.objects.filter(status='PENDING').exists()
+
+    #select_related (ForeignKey, OneToOneField)
+    task_details = TaskDetail.objects.select_related('task').all()
+
+    #prefetch_related
+    task_assigned_to = Task.objects.prefetch_related('assigned_to').all()
+
+    #aggregations
+    total_counts = Task.objects.aggregate(
+    total=Count('id'),
+    pending=Count('id', filter=Q(status='PENDING')),
+    inprogress=Count('id', filter=Q(status='IN_PROGRESS'))
+    )
+
+    #annotation
+    no_of_employee = Task.objects.annotate(num_employees=Count('assigned_to'))
+
 
     context = {
         "tasks": tasks,
-        "task_3": task_3,
+        "task_1": task_1,
         "task_2": task_2,
         "first_task": first_task,
+        "today_task": today_task,
+        "priority_task": priority_task,
+        "tasks_with_c": tasks_with_c,
+        "tasks_pending_or_inprogress": tasks_pending_or_inprogress,
+        "exists_or_not": exists_or_not,
+        "task_details": task_details,
+        "task_assigned_to": task_assigned_to,
+        "total_counts": total_counts,
+        "no_of_employee": no_of_employee
+
     }
 
     return render(request, "show_task.html", context)
+
+
